3c24a128dc5c8b30a2410d0aece5affb94792ebe
Lucy Gelderloos <lucygel@gmail.com>
2022-07-14 15:03:57 -0700

    Revised App & Main

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+// First off, in Main, you'll want to swap out your .forEach() method to use .map() instead. This is actually better, as .map() returns a new array for you so you don't have to create an empty one to push everything into it.
+
+// Next, per directions:
+// Send a function into your Main component that allows the user to update state in the App
+// You'll probably want to move your viewModal state and modal() method from inside of Main and place it in App instead.
+// Next, in addition to having a method that displays a modal, you'll probably also want one that will close it...
+// after that, you'll want to render your SelectedBeast component in App, so you'll probably need to pass some data form App into that component
+// Once you get your beasts rendering again, can successfully render a selected beast, update your time estimates and deploy your site via netlify, I can regrade for full credit
 
 // Send a function into your Main component that allows the user to update state in the App
 // Create a SelectedBeast component and include it in your App
@@ -9,14 +17,28 @@ import Footer from './Footer.js';
 import React from 'react';
 import './App.css';
 import Images from './data.json'
+import SelectedBeast from './SelectedBeast.js';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {modalImage: null, viewModal: false};
+  }
+
+  handleVoteClick = (image) => {
+    this.setState({modalImage: image, viewModal: true});
+  }
+
+  closeModal = () => {
+    this.setState({viewModal: false});
+  }
 
   render() {
       return (
         <>
         <Header />
-        <Main images={Images} />
+        <Main images={Images} handleVoteClick={this.handleVoteClick} />
+        <SelectedBeast image={this.modalImage} closeModal={this.closeModal} viewModal={this.state.viewModal} />
         <Footer />
       </>
       );
--- src/Main.js
+++ src/Main.js
@@ -7,7 +7,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import HornedBeast from "./HornedBeast.js";
 import React from 'react';
-import SelectedBeast from './SelectedBeast.js';
+// import SelectedBeast from './SelectedBeast.js';
 // import Modal from 'react-bootstrap/Modal'
 
 class Main extends React.Component {
@@ -15,37 +15,12 @@ class Main extends React.Component {
     constructor(props) {
       super(props);
       this.images = props.images;
-      // this.handleOpenClick = this.handleOpenClick.bind(this);
-      // this.handleCloseClick = this.handleCloseClick.bind(this);
-      this.state = {viewModal: false}
-      this.displayModal = this.modal;
-    }
-    
-    // handleOpenClick () {
-    //   this.setState({viewModal: true})
-    // }
-
-    // handleCloseClick () {
-    //   this.setState({viewModal: false})
-    // }
-    
-    modal = (image) => {    
-      let modalObject;
-      this.Images.forEach((el) => {
-        if(image === el._id) {
-          modalObject = el;
-        }
-        this.setState({viewModal: true});
-      })
-      return <SelectedBeast image_url={modalObject.image_url} title={modalObject.title} description={modalObject.description} />;
-      // need to pass in key of clicked image, then find that in Images, then call SelectedBeast with the image data as props and have it build a modal that displays because Main's state has also changed from notModal to Modal 
     }
 
     imageCols = () => {
-      let imagesArr = this.images;
       let displayArr = [];
-      imagesArr.forEach((element) => {
-      displayArr.push(<HornedBeast image_url={element.image_url} title={element.title} description={element.description}/>);
+      this.images.forEach((el) => {
+      displayArr.push(<HornedBeast key={el._id} image_url={el.image_url} title={el.title} description={el.description} handleVoteClick={this.props.handleVoteClick} />);
       })
     }
 
@@ -54,7 +29,6 @@ class Main extends React.Component {
 
     return (
         <main>
-          {/* {this.displayModal} */}
           <Container>
             <Row>
               {this.displayArr}
